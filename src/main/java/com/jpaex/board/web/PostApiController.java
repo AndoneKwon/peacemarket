3e3998f2800cffed2fafdc00018a4dfea8e354af
Andone <wlsdn110@gmail.com>
2020-06-01 23:41:16 +0900

    test

--- src/main/java/com/jpaex/board/web/PostApiController.java
+++ src/main/java/com/jpaex/board/web/PostApiController.java
@@ -6,38 +6,30 @@ import com.jpaex.board.web.dto.PostResponseDto;
 import com.jpaex.board.web.dto.PostSaveRequestDto;
 import com.jpaex.board.web.dto.PostUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import net.minidev.json.JSONObject;
-import org.apache.tomcat.util.json.JSONParser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.JsonParseException;
-import org.springframework.boot.json.JsonParser;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-
+/*
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/posts")
 public class PostApiController {
     private PostService postService;
 
-    @PostMapping("/posts/create")
+    @PostMapping("/create")
     public Long save(@RequestBody PostSaveRequestDto requestDto){
         return postService.save(requestDto);
     }
-/*
+
     @RequestMapping(value = "/getPost",method = RequestMethod.GET)
     public PostResponseDto findById(@PathVariable Long id){
         return postService.findById(id);
     }
-*/
-    @GetMapping(value = "/update/")
+
+    @GetMapping(value = "/update/{id}")
     public Long update(@PathVariable Long id,@RequestBody PostUpdateRequestDto requestDto){
         return postService.update(id,requestDto);
-        //return "String";
     }
 
 
@@ -60,3 +52,4 @@ public class PostApiController {
     }
 
 }
+*/
--- src/main/java/com/jpaex/board/web/PostController.java
+++ src/main/java/com/jpaex/board/web/PostController.java
@@ -1,16 +1,48 @@
 package com.jpaex.board.web;
 
 import com.jpaex.board.service.post.PostService;
+import com.jpaex.board.web.dto.PostListResponseDto;
+import com.jpaex.board.web.dto.PostResponseDto;
 import com.jpaex.board.web.dto.PostSaveRequestDto;
+import com.jpaex.board.web.dto.PostUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/posts")
 public class PostController {
     private final PostService postService;
 
+    @PostMapping("/create")
+    public Long save(@RequestBody PostSaveRequestDto requestDto){
+        return postService.save(requestDto);
+    }
+    @PostMapping(value = "/update/{id}")
+    public Long update(@PathVariable("id") Long id, @RequestBody PostUpdateRequestDto requestDto){
+        System.out.println(id);
+        return postService.update(id,requestDto);
+    }
 
+
+    @GetMapping("{id}")
+    public PostResponseDto findById(@PathVariable("id")  Long id){
+        System.out.println(id);
+        return postService.findById(id);
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Long delete(@PathVariable Long id) {
+        postService.delete(id);
+        return id;
+    }
+
+
+    @GetMapping("/getList")
+    public List<PostListResponseDto> findAll() {
+        return postService.findAllDesc();
+    }
 }
